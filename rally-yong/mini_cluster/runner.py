b893f2a600d00b2f9606a4a60dbff540237f1d23
freiris <yongxiang@yongxiangs-MacBook-Pro.local>
2014-09-22 15:34:33 -0700

    rally in openstack instance
    this rally works in an openstack instance, definitely works in a local
    VM. for 10.1.1.180 cluster, you need to explicitly assign the
    shared-internal network to run rally instance, while it is not required
    in 10.1.0.2. After a series of trial-and-error, we found out that in
    10.1.1.180 cluster, ANY user in ANY tenant/project with an ADMIN role
    can launch rally in an EXPLICIT SHARED NON-EXTERNAL network.

---
+++ rally-yong/mini_cluster/runner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import os
+import shutil
+
+for i in range(1,10):
+	#os.system('rally -v task start boot-and-delete.json --tag task-yong')
+        print '************************************%d***************************************'%(i)   
+print 'FINISH'
